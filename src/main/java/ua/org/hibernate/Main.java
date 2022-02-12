d021d4f03568772f3f3963d37b88f22147788390
Oleksandr Pavliuk <a.pavliuk2@gmail.com>
2022-02-12 19:49:21 +0200

    implement logging

--- src/main/java/ua/org/hibernate/Main.java
+++ src/main/java/ua/org/hibernate/Main.java
@@ -1,6 +1,7 @@
 package ua.org.hibernate;
 
 import org.hibernate.Session;
+import ua.org.hibernate.entity.User;
 
 /**
  * @author Alex
@@ -10,6 +11,19 @@ public class Main {
   public static void main(String[] args) {
 
     Session session = HibernateUtil.getSessionFactory().openSession();
+
+    session.getTransaction().begin();
+
+    User user = new User();
+    user.setEmail("newfromapap5s2@email.com");
+    user.setUsername("newfromapps5aa");
+    user.setPassword("2asdfsdfsdfaa");
+
+    session.save(user);
+
+    session.getTransaction().commit();
+
+    System.out.println("user.getId() = " + user.getId());
     session.close();
     HibernateUtil.close();
   }
